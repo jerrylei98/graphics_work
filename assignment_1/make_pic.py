d83469422f29fe661cbde65ceec9b8285a9beae1
jerrylei98 <jerrylei98@gmail.com>
2016-02-04 11:01:14 -0500

    first assignment

---
+++ assignment_1/make_pic.py
@@ -0,0 +1,26 @@
+FILE_NAME = "assignment_1.ppm"
+IMAGE_TYPE = "P3"
+X_MAX = 500
+Y_MAX = 500
+MAX_COLOR = 255
+
+
+fd = open("../../image_files/" + FILE_NAME, "w")
+fd.write(IMAGE_TYPE + " " + str(X_MAX) + " " + str(Y_MAX) + " " + str(MAX_COLOR))
+
+cX = 0
+cY = 0
+
+while(cY < Y_MAX):
+    while(cX < X_MAX):
+          r = 
+          g =
+          b =
+          fd.write(" " + str(r) + " " + str(g) + " " + str(b))
+          cX+=1
+    cY += 1
+
+    
+          
+        
+
