eab79775c7bafb806b70f3fbfcc27809874b496d
jerrylei98 <jerrylei98@gmail.com>
2016-02-05 10:53:52 -0500

    fixed template

--- template/TEMPLATE.py
+++ template/TEMPLATE.py
@@ -1,3 +1,10 @@
+#--Checks if argument is 'imgur'--
+import sys
+if len(sys.argv) > 1:
+    go = sys.argv[1]
+else:
+    go = ""
+
 #--Creates ppm file-- 
 
 FILE_NAME = "" #no need for .ppm
@@ -6,8 +13,11 @@ X_MAX = 500
 Y_MAX = 500
 MAX_COLOR = 255
 
+if go == "imgur": #if imgur is an argument
+    fd = open("../../image_files/" + FILE_NAME + ".ppm", "w")
+else:
+    fd = open(FILE_NAME + ".ppm", 'w')
 
-fd = open("../../image_files/" + FILE_NAME + ".ppm", "w")
 fd.write(IMAGE_TYPE + " " + str(X_MAX) + " " + str(Y_MAX) + " " + str(MAX_COLOR))
 
 #<--Drawing goes here-->#
@@ -16,14 +26,14 @@ fd.write(IMAGE_TYPE + " " + str(X_MAX) + " " + str(Y_MAX) + " " + str(MAX_COLOR)
 fd.close()
 
 #--Executes upload to imgur--
+if go == "imgur":
+    import subprocess
+    
+    cmd = "../scripts/upload.sh " + FILE_NAME
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) #found on stackoverflow
+    p.wait()
 
-import subprocess
-
-cmd = "../scripts/upload.sh " + FILE_NAME
-p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) #found on stackoverflow
-p.wait()
-
-#read the imgur link
-fd = open("../scripts/imgur_return.txt", "r")
-print '\n' + fd.read() + '\n'
-fd.close()        
+    #read the imgur link
+    fd = open("../scripts/imgur_return.txt", "r")
+    print '\n' + fd.read() + '\n'
+    fd.close()        
