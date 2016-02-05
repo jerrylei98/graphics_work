887351cf7feefaa7a740adc038ac6db3a045a864
jerrylei98 <jerrylei98@gmail.com>
2016-02-05 10:46:57 -0500

    arg works check no arg on local

--- assignment_1/make_pic.py
+++ assignment_1/make_pic.py
@@ -1,12 +1,16 @@
+import sys
+go = 'imgur'
+
 FILE_NAME = "assignment_1" #no need for .ppm
 IMAGE_TYPE = "P3"
 X_MAX = 500
 Y_MAX = 500
 MAX_COLOR = 255
 
-
-#fd = open("../../image_files/" + FILE_NAME + ".ppm", "w")
-fd = open(FILE_NAME + ".ppm", 'w')
+if go == "imgur":
+    fd = open("../../image_files/" + FILE_NAME + ".ppm", "w")
+else:
+    fd = open(FILE_NAME + ".ppm", 'w')
 fd.write(IMAGE_TYPE + " " + str(X_MAX) + " " + str(Y_MAX) + " " + str(MAX_COLOR))
 
 cY = 0
@@ -22,16 +26,15 @@ while(cY < Y_MAX):
     cY += 1
 
 fd.close()
-          
-"""
-import subprocess
 
-cmd = "../scripts/upload.sh " + FILE_NAME
-p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) #found on stackoverflow
-p.wait()
+if go == "imgur":
+    import subprocess
+    cmd = "../scripts/upload.sh " + FILE_NAME
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) #found on stackoverflow
+    p.wait()
 
-#read the imgur link
-fd = open("../scripts/imgur_return.txt", "r")
-print '\n' + fd.read() + '\n'
-fd.close()        
-"""
+    #read the imgur link
+    fd = open("../scripts/imgur_return.txt", "r")
+    print '\n' + fd.read() + '\n'
+    fd.close()        
+
