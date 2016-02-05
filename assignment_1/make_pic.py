56393def37ecf269e0ec51b09725d68c65e3ef89
jerrylei98 <jerrylei98@gmail.com>
2016-02-04 19:17:31 -0500

    kinda broken assignment_1

--- assignment_1/make_pic.py
+++ assignment_1/make_pic.py
@@ -9,15 +9,15 @@ fd = open("../../image_files/" + FILE_NAME, "w")
 #fd = open(FILE_NAME, 'w')
 fd.write(IMAGE_TYPE + " " + str(X_MAX) + " " + str(Y_MAX) + " " + str(MAX_COLOR))
 
-cX = 0
 cY = 0
 
 while(cY < Y_MAX):
+    cX = 0
     while(cX < X_MAX):
           r = X_MAX - cX - MAX_COLOR
-          g = X_MAX - cX - MAX_COLOR
-          b = X_MAX - cX - MAX_COLOR
-          fd.write(" " + str(r) + " " + str(g) + " " + str(b))
+#          g = (cX * (MAX_COLOR / X_MAX) % MAX_COLOR)
+#          b = (cX * (MAX_COLOR / X_MAX) % MAX_COLOR)
+          fd.write(" " + str(r) + " " + str(r) + " " + str(r))
           cX+=1
     cY += 1
 
