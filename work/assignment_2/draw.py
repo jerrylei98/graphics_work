7730d61a8d84bff042e1192cb8dccd82eb86d5f9
jerrylei98 <jerrylei98@gmail.com>
2016-02-12 10:53:16 -0500

    base of drawline setup

--- work/assignment_2/draw.py
+++ work/assignment_2/draw.py
@@ -1,4 +1,14 @@
 from display import *
 
 def draw_line( screen, x0, y0, x1, y1, color ):
-    pass
+    m = (y1 - y0) / (x1 - x0)
+    #QUADRANT 1
+    #OCTANT I
+    if x0 > 0 & x1 > 0 & y0 > 0 & y1 > 0:
+        if 0 < m < 1:
+            print 'O1'
+        else if m > 1:
+            print 'O2'
+        print 'Q1'
+
+
