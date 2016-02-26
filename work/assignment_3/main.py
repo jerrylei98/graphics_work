b871877d6afb9b8acacb621b48b474f7486aff32
jerrylei98 <jerrylei98@gmail.com>
2016-02-26 09:35:37 -0500

    assignment_3 base ready

---
+++ work/assignment_3/main.py
@@ -0,0 +1,47 @@
+from sys import argv
+from display import * 
+from draw import *
+
+#--Checks if argument is 'imgur'--
+
+if len(argv) > 1:
+    go = argv[1]
+    FILE_NAME = "assignment_2"
+    fname = "../../../image_files/" + FILE_NAME + ".ppm"
+else:
+    go = ""
+
+#-----------
+#constant
+
+
+
+screen = new_screen()
+
+color = [ 0, 255, 0 ]
+
+draw_line(screen
+
+
+
+#---------------------------------------------------------------#
+
+if go != "imgur":
+    display(screen)
+
+#--Executes upload to imgur--
+if go == "imgur":
+
+    from subprocess import Popen, PIPE
+    save_ppm(screen, fname)
+    cmd = "../../scripts/upload.sh " + FILE_NAME
+    p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+    p.wait()
+
+
+    #read the imgur link
+    fd = open("../../scripts/imgur_return.txt", "r")
+    print '\n' + fd.read() + '\n'
+    fd.close()  
+      
+#------ file end
