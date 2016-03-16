b985676ef1a236b8c955adfde8098f8eacc18328
jerrylei98 <jerrylei98@gmail.com>
2016-03-16 10:38:09 -0400

    basics for parser done

--- work/assignment_4/parser.py
+++ work/assignment_4/parser.py
@@ -3,5 +3,19 @@ from matrix import *
 from draw import *
 
 def parse_file( fname, points, transform, screen, color ):
-    pass
+    f1 = open(fname, 'r')
+    d = f1.read().split('\n')
+    c1 = 0
+    while c1 < len(d):
+        if c1 == 'circle':
+            d1 = d[c1+1].split(' ')
+            circle(d1[0], d1[1], d1[2]) #use proper command
+        elif c1 == 'hermite':
+            d1 = d[c1+1].split(' ')
+        elif c1 == 'hermite':
+            d1 = d[c1+1].split(' ')
+        elif c1 == 'hermite':
+            d1 = d[c1+1].split(' ')
+        c1 += 1
+        
 
